987ed435f238b3fafc9a4777b2a43572fa18e4ec
haiaza <ajames1342@gmail.com>
2024-07-27 22:12:04 -0400

    Model + Server Boilerplate

---
+++ server.js
@@ -0,0 +1,21 @@
+const dotenv = require('dotenv')
+dotenv.config() // loading the enviornment variables
+//server requirements
+const express = require('express')
+const logger = require('morgan')
+const mongoose = require('mongoose')
+const MONGODB_URI = process.env.MONGODB_URI
+//initialize the server
+const app = express()
+app.use(morgan('dev'))
+
+const Movie = require('./models/movie')
+
+mongoose.connect(MONGODB_URI)
+mongoose.connection.on('connected', () =>{
+    console.log(`Connected to MongoDB ${mongoose.connection.name}`)
+})
+
+app.listen(3000, () =>{
+    console.log('Port 3000 is active')
+})
